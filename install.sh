526a49bec57c872b5dee1d1548c9e938f8c0db7a
Jing-Yuan Huang <59347832+Kevin75311@users.noreply.github.com>
2025-04-08 18:39:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-conda install opencv
+conda create -y -n week2_new python=3.12
+conda init --all
+source /home/pi/miniconda3/etc/profile.d/conda.sh
+conda activate week2_new
+
+conda install opencv -y
 conda install -c conda-forge libstdcxx-ng -y
 
 sudo apt update
 sudo apt install -y libcamera-dev
-pip install rpi-libcamera
+pip install -y rpi-libcamera
 
 sudo apt install -y libkms++-dev libfmt-dev libdrm-dev
-pip install rpi-kms
+pip install -y rpi-kms
 
 sudo apt-get install -y libcap-dev
-pip install picamera2
+pip install -y picamera2
